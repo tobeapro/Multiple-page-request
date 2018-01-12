a10eab17c32c2cb7c32b1d3b0240bc3b0158a71d
rk <471091127@qq.com>
2018-01-12 17:28:20 +0800

    koa-app

--- koa-app.js
+++ koa-app.js
@@ -1,6 +1,6 @@
 const koa=require('koa')
 const route=require('koa-route')
-const timeout=require('koa-timeout')(100000)
+const timeout=require('koa-timeout')(1000000)
 const agent=require('superagent')
 const cheerio=require('cheerio')
 const port=8088
@@ -17,45 +17,58 @@ function requestInfo(URL){
 	})
 }
 app.use(timeout)
-app.use(route.get('/',async (ctx,next)=>{
+app.use(route.get('/',(ctx,next)=>{
 	console.time('requestTime')
 	agent.get('http://www.casad.cas.cn/chnl/371/index.html')
-	.end((err,data)=>{
+	.end(async (err,data)=>{
 		if(err){
 			next(err)
 		}
 		let $=cheerio.load(data.text)
 		allLink=$("#allNameBar a")
+		allLink=allLink[10]
 		let allUrl=[]
-		allLink.each((index,item)=>{
-			if(index<3){
-				return
-			}else{
-				let url=$(item).attr('href')
-				allUrl.push(await requestInfo(url))		
-				next()	
-			}	
-		})
-		allUrl=allUrl.slice(0,10)	
-		await Promise.all(allUrl)
-		.then((pages)=>{
-			let infoData=[]
-			pages.forEach((html)=>{
-				let $=cheerio.load(html.text)
-				let info={}
-				let name=$('.contentBar .title h1').text()
-				info.name=name
-				let intro=$("#zoom p")		
-				info.intro=intro.text()				
-				infoData.push(info)
-			})
-			console.timeEnd('requestTime')
-			ctx.body=infoData
+		console.log(allLink)
+		// allLink.each((index,item)=>{
+		// 	if(index<3){
+		// 		return
+		// 	}else{
+		// 		let url=$(item).attr('href')
+		// 		allUrl.push(requestInfo(url))		
+		// 		return	
+		// 	}	
+		// })
+		// for(let i=0;i<allLink.length;i++){
+		// 	if(i<3){
+		// 		continue
+		// 	}else{
+		// 		let url=allLink[i].attr('href')
+		// 		allUrl.push(await requestInfo(url))
+		// 		continue
+		// 	}
+		// }
+		let url=allLink.attr('href')
+		allUrl.push(await requestInfo(url))
+		ctx.body=allUrl
+		// Promise.all(allUrl)
+		// .then((pages)=>{
+		// 	let infoData=[]
+		// 	pages.forEach((html)=>{
+		// 		let $=cheerio.load(html.text)
+		// 		let info={}
+		// 		let name=$('.contentBar .title h1').text()
+		// 		info.name=name
+		// 		let intro=$("#zoom p")		
+		// 		info.intro=intro.text()				
+		// 		infoData.push(info)
+		// 	})
+		// 	console.timeEnd('requestTime')
+		// 	ctx.body=infoData
 			
-		})
-		.catch((erro)=>{
-			ctx.body=erro
-		})
+		// })
+		// .catch((erro)=>{
+		// 	ctx.body=erro
+		// })
 	})
 }))
 app.listen(port,()=>{
